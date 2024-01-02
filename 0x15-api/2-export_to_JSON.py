5f6f60462972f3f493bdef4789b63e601a8139da
nokekip <kipnoke@gmail.com>
2024-01-02 22:11:50 +0300

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Fetch and Export to JSON employee TODO list progress using a REST API.
+"""
+import json
+import requests
+import sys
+
+
+def export_to_json():
+    """return API data"""
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    user = requests.get(base_url + f"users/{user_id}").json()
+    todos = requests.get(base_url + f"users/{user_id}/todos").json()
+
+    """Export data to JSON"""
+    with open(f"{user_id}.json", 'w') as jsonfile:json.dump({user_id: [{
+            "task": todo.get('title'),
+            "completed": todo.get('completed'),
+            "username": user.get('username')
+        }for todo in todos]}, jsonfile)
+
+
+if __name__ == '__main__':
+    export_to_json()
