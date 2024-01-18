e94334f33227f9f2eaf5bb7ee22e1b52ac54abde
nokekip <kipnoke@gmail.com>
2024-01-18 22:11:46 +0300

    Fix php extension issue in wp-settings.php

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix a php extension
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
